bbd9e2937d90c8e7f04c8242b78d9f942ad9b988
konics556 <kanishkpatidar18@gmail.com>
2021-08-19 17:28:25 +0530

    UI changes

--- src/common/Grid/GridPhoto/GridPhoto.tsx
+++ src/common/Grid/GridPhoto/GridPhoto.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 function GridPhoto(props: {photo: string, likes: number, description: string}) {
     return (
         <div className="grid-photo">
+            <div className="grid-likes"><span>{props.likes} Likes</span></div>
             <img src={props.photo} alt={props.description} />
-            {/* <div className="grid-likes">{props.likes}</div> */}
         </div>
     )
 }
--- src/common/Loading/Loading.tsx
+++ src/common/Loading/Loading.tsx
@@ -1,7 +1,13 @@
+import './Loading.css';
+
+import { PulseLoader } from 'react-spinners';
+
 const Loading = () => {
     return (
         <div className="loading">
-            Loading
+            <div className="loading-container">
+                <PulseLoader color={'lightgray'} css="margin: 50px;" size={30} />
+            </div>
         </div>
     )
 }
